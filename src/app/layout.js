faef1da148acfe24ca166abc2d070823dfb104c0
subhash pal <palsubhash046@gmail.com>
2026-03-31 21:01:45 +0530

    add session provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner"
+import Provider from "@/provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +25,12 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        {children}
+        
+        <Provider>
+
+          {children}
+          
+        </Provider>
 
         <Toaster />
       </body>
