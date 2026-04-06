aa77eb0fecbcfb77c4fb6c80480557e0f9d7d1de
subhash pal <palsubhash046@gmail.com>
2026-04-06 14:48:25 +0530

    add GetCurrentUser

---
+++ src/AppInit.js
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+import GetCurrentUser from './customHooks/GetCurrentUser'
+
+const AppInit = ({ children }) => {
+       return (
+              <>
+                     <GetCurrentUser />
+                     {children}
+              </>
+       )
+}
+
+export default AppInit
