406b1efc2613fd913fe8d20ca0d2d63cd89e1077
subhash pal <palsubhash046@gmail.com>
2026-04-06 14:49:30 +0530

    get currentUser

---
+++ src/customHooks/GetCurrentUser.js
@@ -0,0 +1,31 @@
+"use client"
+import { setUserData } from "@/redux/userSlice"
+import axios from "axios"
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+
+const GetCurrentUser = () => {
+
+       const dispatch = useDispatch()
+
+       useEffect(() => {
+              const fetchUser = async () => {
+                     try {
+                            const res = await axios.get("/api/currentUser")
+                            console.log("response:", res)
+
+                            if (res.data.success) {
+                                   dispatch(setUserData(res.data.data))
+                            }
+                     } catch (error) {
+                            console.log("get current-user error:", error)
+                     }
+              }
+
+              fetchUser()
+       }, [dispatch]) 
+
+       return null 
+}
+
+export default GetCurrentUser
