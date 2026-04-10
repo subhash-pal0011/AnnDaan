e843416330e1d92b44be129c1233cac9f1abe4b1
subhash pal <palsubhash046@gmail.com>
2026-04-10 21:13:29 +0530

    add socketConnection

--- src/AppInit.js
+++ src/AppInit.js
@@ -1,14 +1,54 @@
-"use client"
-import React from 'react'
-import GetCurrentUser from './customHooks/GetCurrentUser'
+// "use client"
+// import React from 'react'
+// import GetCurrentUser from './customHooks/GetCurrentUser'
+
+// const AppInit = ({ children }) => {
+//        return (
+//               <>
+//                      <GetCurrentUser />
+//                      {children}
+//               </>
+//        )
+// }
+
+// export default AppInit
+
+
+"use client";
+import React, { useEffect } from "react";
+import GetCurrentUser from "./customHooks/GetCurrentUser";
+import { useSelector } from "react-redux";
+import { socketConnection } from "./lib/socketConnection";
 
 const AppInit = ({ children }) => {
+       const userId = useSelector((state) => state.user.userData?._id);
+
+       useEffect(() => {
+              if (!userId) return;
+
+              const socket = socketConnection();
+
+              const handleConnect = () => {
+                     socket.emit("userId", userId);
+              };
+
+              socket.on("connect", handleConnect);
+
+              if (socket.connected) {
+                     handleConnect();
+              }
+
+              return () => {
+                     socket.off("connect", handleConnect);
+              };
+       }, [userId]);
+
        return (
               <>
                      <GetCurrentUser />
                      {children}
               </>
-       )
-}
+       );
+};
 
-export default AppInit
+export default AppInit;
