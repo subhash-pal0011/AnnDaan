b57c5e0b233afdb1c7ff43739358a27012246acf
subhash pal <palsubhash046@gmail.com>
2026-04-10 21:18:20 +0530

    correct and complete connectSocket

--- src/app/api/socket/connectSocket/route.js
+++ src/app/api/socket/connectSocket/route.js
@@ -3,52 +3,15 @@ import User from "@/model/user";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-       try {
-              await connectDb();
+       await connectDb();
 
-              const { userId, socketId } = await req.json();
+       const { userId, socketId } = await req.json();
 
-              if (!userId) {
-                     return NextResponse.json(
-                            { success: false, message: "UserId missing" },
-                            { status: 400 }
-                     );
-              }
+       await User.findByIdAndUpdate(userId, {
+              socketId,
+              isOnline: true,
+              lastSeen: new Date(),
+       });
 
-              const user = await User.findByIdAndUpdate(
-                     userId,
-                     {
-                            socketId,
-                            isOnline: true,
-                     },
-                     { new: true }
-              );
-
-              if (!user) {
-                     return NextResponse.json(
-                            { success: false, message: "User not exist" },
-                            { status: 404 }
-                     );
-              }
-
-              return NextResponse.json(
-                     {
-                            success: true,
-                            message: "User online updated",
-                            data:user,
-                     },
-                     { status: 200 }
-              );
-
-       } catch (error) {
-              console.error("Socket update error:", error);
-
-              return NextResponse.json(
-                     {
-                            success: false,
-                            message: "Internal Server Error",
-                     },
-                     { status: 500 }
-              );
-       }
+       return NextResponse.json({ success: true });
 }
